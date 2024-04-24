2b44dcd2c8a7d7a43253782ae535130f901f51d4
Tialo <tialox@yandex.ru>
2024-04-24 22:38:14 +0300

    db iter

--- init_db.sql
+++ init_db.sql
@@ -1,20 +1,36 @@
---TODO: Добавить таблицу country
+create table country(
+    id serial primary key,
+    full_name varchar(32),
+    short_name varchar(8)
+);
+insert into country (full_name, short_name)
+values
+    ('Russia', 'RU'),
+    ('United States of America', 'USA'),
+    ('France', 'FR'),
+    ('Germany', 'DE'),
+    ('Australia', 'AU'),
+    ('China', 'CN'),
+    ('Canada', 'CA'),
+    ('United Kingdom', 'UK');
+
 create table authors(
     id serial primary key,
     name varchar(256),
     last_name varchar(256),
-    birth_date date
+    birth_date date,
+    country_id int references country(id)
 );
-INSERT INTO authors (name, last_name, birth_date)
+INSERT INTO authors (name, last_name, birth_date, country_id)
 VALUES
-    ('J.R.R.', 'Tolkien', '1892-01-03'),
-    ('Leo', 'Tolstoy', '1828-09-09'),
-    ('Mark', 'Twain', '1835-11-30'),
-    ('Charles', 'Dickens', '1812-02-07'),
-    ('Virginia', 'Woolf', '1882-01-25'),
-    ('Harper', 'Lee', '1926-04-28'),
-    ('F. Scott', 'Fitzgerald', '1896-09-24'),
-    ('Ernest', 'Hemingway', '1899-07-21');
+    ('J.R.R.', 'Tolkien', '1892-01-03', 8),
+    ('Leo', 'Tolstoy', '1828-09-09', 1),
+    ('Mark', 'Twain', '1835-11-30', 2),
+    ('Charles', 'Dickens', '1812-02-07', 8),
+    ('Virginia', 'Woolf', '1882-01-25', 2),
+    ('Harper', 'Lee', '1926-04-28', 3),
+    ('F. Scott', 'Fitzgerald', '1896-09-24', 5),
+    ('Ernest', 'Hemingway', '1899-07-21', 2);
 
 create table genres(
     id serial primary key,
@@ -34,24 +50,43 @@ VALUES
 create table book_base(
     id serial primary key,
     name varchar(256),
-    genre_id int references genres(id),
     isbn varchar(256),
     write_date date
 );
-INSERT INTO book_base (name, genre_id, isbn, write_date)
+INSERT INTO book_base (name, isbn, write_date)
 VALUES
-    ('The Lord of the Rings', 1, '9780618640157', '1954-07-29'),
-    ('The Hobbit', 1, '9780547928227', '1937-09-21'),
-    ('War and Peace', 2, '9780199232765', '1869-01-01'),
-    ('Anna Karenina', 5, '9780143035008', '1877-01-01'),
-    ('The Adventures of Huckleberry Finn', 5, '9780199536559', '1884-12-10'),
-    ('Oliver Twist', 5, '9780141439747', '1837-01-01'),
-    ('To the Lighthouse', 5, '9780156907392', '1927-05-05'),
-    ('Mrs. Dalloway', 5, '9780156030359', '1925-05-14'),
-    ('To Kill a Mockingbird', 5, '9780446310789', '1960-07-11'),
-    ('The Great Gatsby', 5, '9780743273565', '1925-04-10'),
-    ('For Whom the Bell Tolls', 5, '9780684803357', '1940-10-21'),
-    ('A Farewell to Arms', 5, '9780684801469', '1929-09-27');
+    ('The Lord of the Rings', '9780618640157', '1954-07-29'),
+    ('The Hobbit', '9780547928227', '1937-09-21'),
+    ('War and Peace', '9780199232765', '1869-01-01'),
+    ('Anna Karenina', '9780143035008', '1877-01-01'),
+    ('The Adventures of Huckleberry Finn', '9780199536559', '1884-12-10'),
+    ('Oliver Twist', '9780141439747', '1837-01-01'),
+    ('To the Lighthouse', '9780156907392', '1927-05-05'),
+    ('Mrs. Dalloway', '9780156030359', '1925-05-14'),
+    ('To Kill a Mockingbird', '9780446310789', '1960-07-11'),
+    ('The Great Gatsby', '9780743273565', '1925-04-10'),
+    ('For Whom the Bell Tolls', '9780684803357', '1940-10-21'),
+    ('A Farewell to Arms', '9780684801469', '1929-09-27');
+
+create table book_base_genres(
+    id serial primary key,
+    book_base_id int references book_base(id),
+    genre_id int references genres(id)
+);
+insert into book_base_genres (book_base_id, genre_id)
+values
+    (1, 1),
+    (2, 1),
+    (3, 2),
+    (4, 5),
+    (5, 5),
+    (6, 5),
+    (7, 5),
+    (8, 5),
+    (9, 5),
+    (10, 5),
+    (11, 5),
+    (12, 5);
 
 create table books_authors(
     id serial primary key,
@@ -77,18 +112,18 @@ create table publishers(
     id serial primary key,
     name varchar(256),
     description varchar(256),
-    country varchar(256), --TODO: country tablica
+    country_id int references country(id),
     contacts varchar(256)
 );
-INSERT INTO publishers (name, description, country, contacts)
+INSERT INTO publishers (name, description, country_id, contacts)
 VALUES
-    ('Penguin Random House', 'Publishing company known for a wide range of books.', 'United States', 'contact@penguinrandomhouse.com'),
-    ('HarperCollins Publishers', 'One of the world largest publishing companies.', 'United States', 'info@harpercollins.com'),
-    ('Simon & Schuster', 'Global publishing company known for bestsellers and literary fiction.', 'United States', 'info@simonandschuster.com'),
-    ('Macmillan Publishers', 'Global trade publishing company.', 'United Kingdom', 'info@macmillan.com'),
-    ('Hachette Livre', 'Largest publishing company in France and one of the largest in the world.', 'France', 'info@hachette.fr'),
-    ('Pearson plc', 'Multinational publishing and education company.', 'United Kingdom', 'contact@pearson.com'),
-    ('Oxford University Press', 'Largest university press in the world.', 'United Kingdom', 'info@oup.com');
+    ('Penguin Random House', 'Publishing company known for a wide range of books.', 2, 'contact@penguinrandomhouse.com'),
+    ('HarperCollins Publishers', 'One of the world largest publishing companies.', 2, 'info@harpercollins.com'),
+    ('Simon & Schuster', 'Global publishing company known for bestsellers and literary fiction.', 2, 'info@simonandschuster.com'),
+    ('Macmillan Publishers', 'Global trade publishing company.', 8, 'info@macmillan.com'),
+    ('Hachette Livre', 'Largest publishing company in France and one of the largest in the world.', 3, 'info@hachette.fr'),
+    ('Pearson plc', 'Multinational publishing and education company.', 8, 'contact@pearson.com'),
+    ('Oxford University Press', 'Largest university press in the world.', 8, 'info@oup.com');
 
 create table book_publisher(
     id serial primary key,
@@ -207,3 +242,89 @@ VALUES
     (13, 'Interesting premise, but execution fell short.', 3),
     (14, 'Couldn''t connect with the characters. Story lacked depth.', 2),
     (15, 'Compelling and thought-provoking. Left me wanting more.', 4);
+
+create table user_description(
+    id serial primary key,
+    email varchar(64),
+    password varchar(64),
+    join_date date,
+    bio varchar(512),
+    country_id int references country(id),
+    user_id int references users(id)
+);
+INSERT INTO user_description (email, password, join_date, bio, country_id, user_id)
+VALUES
+    ('user1@example.com', MD5('password1'), '2023-01-01', 'Bio for User 1', 1, 1),
+    ('user2@example.com', MD5('password2'), '2023-02-02', 'Bio for User 2', 2, 2),
+    ('user3@example.com', MD5('password3'), '2023-03-03', 'Bio for User 3', 3, 3),
+    ('user4@example.com', MD5('password4'), '2023-04-04', 'Bio for User 4', 4, 4),
+    ('user5@example.com', MD5('password5'), '2023-05-05', 'Bio for User 5', 5, 5),
+    ('user6@example.com', MD5('password6'), '2023-06-06', 'Bio for User 6', 6, 6),
+    ('user7@example.com', MD5('password7'), '2023-07-07', 'Bio for User 7', 7, 7),
+    ('user8@example.com', MD5('password8'), '2023-08-08', 'Bio for User 8', 1, 8),
+    ('user9@example.com', MD5('password9'), '2023-09-09', 'Bio for User 9', 2, 9),
+    ('user10@example.com', MD5('password10'), '2023-10-10', 'Bio for User 10', 3, 10),
+    ('user11@example.com', MD5('password11'), '2023-11-11', 'Bio for User 11', 4, 11),
+    ('user12@example.com', MD5('password12'), '2023-12-12', 'Bio for User 12', 5, 12),
+    ('user13@example.com', MD5('password13'), '2024-01-01', 'Bio for User 13', 6, 13);
+
+
+create table bookmarks(
+    id serial primary key,
+    user_edition_id int references user_edition(id),
+    page int
+);
+INSERT INTO bookmarks (user_edition_id, page)
+VALUES
+    (1, 10),
+    (2, 20),
+    (3, 30),
+    (4, 40),
+    (5, 50),
+    (6, 60),
+    (7, 70);
+
+create table status(
+    id serial primary key,
+    status varchar(32)
+);
+INSERT INTO status (status)
+VALUES
+    ('Reading'),
+    ('To Read'),
+    ('Completed'),
+    ('Paused'),
+    ('Abandoned'),
+    ('Reviewing'),
+    ('Planning');
+
+create table book_status(
+    id serial primary key,
+    user_edition_id int references user_edition(id),
+    status_id int references status(id)
+);
+insert into book_status (user_edition_id, status_id)
+values
+    (4, 1),
+    (1, 2),
+    (6, 3),
+    (7, 4),
+    (11, 5),
+    (2, 6),
+    (9, 7);
+
+create table transactions(
+    id serial primary key,
+    type varchar(16),
+    table_name varchar(32),
+    record_id int
+);
+insert into transactions (type, table_name, record_id)
+values
+    ('inserted', 'book_status', 1),
+    ('inserted', 'book_status', 2),
+    ('inserted', 'book_status', 3),
+    ('inserted', 'book_status', 4),
+    ('inserted', 'book_status', 5),
+    ('inserted', 'book_status', 6),
+    ('inserted', 'book_status', 7);
