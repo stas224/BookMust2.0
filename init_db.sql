684c47de472f12b830cec39d80e68ad6ef7e9d82
Stanislav Belosludtsev <stas224@yandex-team.ru>
2024-04-27 02:20:06 +0300

    translate db to ru

--- init_db.sql
+++ init_db.sql
@@ -6,14 +6,14 @@ create table country(
 );
 insert into country (full_name, short_name, flag_url)
 values
-    ('Russia', 'RU', 'flag_ru.png'),
-    ('United States of America', 'US', 'flag_usa.png'),
-    ('France', 'FR', 'flag_fr.png'),
-    ('Germany', 'DE', 'flag_de.png'),
-    ('Australia', 'AU', 'flag_au.png'),
-    ('China', 'CN', 'flag_cn.png'),
-    ('Canada', 'CA', 'flag_ca.png'),
-    ('United Kingdom', 'UK', 'flag_uk.png');
+    ('Россия', 'RU', 'flag_ru.png'),
+    ('Соединенные Штаты Америки', 'US', 'flag_usa.png'),
+    ('Франция', 'FR', 'flag_fr.png'),
+    ('Германия', 'DE', 'flag_de.png'),
+    ('Австралия', 'AU', 'flag_au.png'),
+    ('Китай', 'CN', 'flag_cn.png'),
+    ('Канада', 'CA', 'flag_ca.png'),
+    ('Великобритания', 'UK', 'flag_uk.png');
 
 create table authors(
     id serial primary key,
@@ -24,14 +24,14 @@ create table authors(
 );
 INSERT INTO authors (name, last_name, birth_date, country_id)
 VALUES
-    ('J.R.R.', 'Tolkien', '1892-01-03', 8),
-    ('Leo', 'Tolstoy', '1828-09-09', 1),
-    ('Mark', 'Twain', '1835-11-30', 2),
-    ('Charles', 'Dickens', '1812-02-07', 8),
-    ('Virginia', 'Woolf', '1882-01-25', 2),
-    ('Harper', 'Lee', '1926-04-28', 3),
-    ('F. Scott', 'Fitzgerald', '1896-09-24', 5),
-    ('Ernest', 'Hemingway', '1899-07-21', 2);
+    ('Дж.Р.Р.', 'Толкиен', '1892-01-03', 8),
+    ('Лев', 'Толстой', '1828-09-09', 1),
+    ('Марк', 'Твен', '1835-11-30', 2),
+    ('Чарльз', 'Диккенс', '1812-02-07', 8),
+    ('Вирджиния', 'Вулф', '1882-01-25', 2),
+    ('Харпер', 'Ли', '1926-04-28', 3),
+    ('Ф. Скотт', 'Фицджеральд', '1896-09-24', 5),
+    ('Эрнест', 'Хемингуэй', '1899-07-21', 2);
 
 create table genres(
     id serial primary key,
@@ -40,13 +40,13 @@ create table genres(
 );
 INSERT INTO genres (name, description)
 VALUES
-    ('Fantasy', 'Imaginative fiction set in alternate worlds or realities.'),
-    ('Historical Fiction', 'Fiction set in the past, often featuring real historical events and figures.'),
-    ('Mystery', 'A genre of fiction that deals with the solution of a crime or the unraveling of secrets.'),
-    ('Science Fiction', 'Fiction that deals with imaginative concepts such as futuristic science and technology.'),
-    ('Romance', 'A genre focused on romantic love stories between characters.'),
-    ('Thriller', 'A genre characterized by fast-paced plots, frequent action, and danger.'),
-    ('Horror', 'A genre of fiction intended to frighten, scare, or disgust the reader/viewer.');
+    ('Фэнтези', 'Жанр художественной литературы, действие которого происходит в вымышленных мирах или реальностях.'),
+    ('Историческая проза', 'Жанр художественной литературы, действие которой разворачивается в прошлом, часто с участием реальных исторических событий и фигур.'),
+    ('Детектив', 'Жанр художественной литературы, который связан с решением преступления или разгадыванием тайн.'),
+    ('Научная фантастика', 'Жанр художественной литературы, который затрагивает фантастические концепции, такие как будущие научные и технологические достижения.'),
+    ('Романтика', 'Жанр, сосредоточенный на историях романтической любви между персонажами.'),
+    ('Триллер', 'Жанр, характеризующийся быстро развивающимся сюжетом, частыми действиями и опасностью.'),
+    ('Ужасы', 'Жанр художественной литературы, предназначенный для того, чтобы напугать, испугать или вызвать отвращение у читателя/зрителя.');
 
 create table book_base(
     id serial primary key,
@@ -56,18 +56,18 @@ create table book_base(
 );
 INSERT INTO book_base (name, isbn, write_date)
 VALUES
-    ('The Lord of the Rings', '9780618640157', '1954-07-29'),
-    ('The Hobbit', '9780547928227', '1937-09-21'),
-    ('War and Peace', '9780199232765', '1869-01-01'),
-    ('Anna Karenina', '9780143035008', '1877-01-01'),
-    ('The Adventures of Huckleberry Finn', '9780199536559', '1884-12-10'),
-    ('Oliver Twist', '9780141439747', '1837-01-01'),
-    ('To the Lighthouse', '9780156907392', '1927-05-05'),
-    ('Mrs. Dalloway', '9780156030359', '1925-05-14'),
-    ('To Kill a Mockingbird', '9780446310789', '1960-07-11'),
-    ('The Great Gatsby', '9780743273565', '1925-04-10'),
-    ('For Whom the Bell Tolls', '9780684803357', '1940-10-21'),
-    ('A Farewell to Arms', '9780684801469', '1929-09-27');
+    ('Властелин колец', '9780618640157', '1954-07-29'),
+    ('Хоббит, или Туда и обратно', '9780547928227', '1937-09-21'),
+    ('Война и мир', '9780199232765', '1869-01-01'),
+    ('Анна Каренина', '9780143035008', '1877-01-01'),
+    ('Приключения Гекльберри Финна', '9780199536559', '1884-12-10'),
+    ('Оливер Твист', '9780141439747', '1837-01-01'),
+    ('К маяку', '9780156907392', '1927-05-05'),
+    ('Миссис Дэллоуэй', '9780156030359', '1925-05-14'),
+    ('Убить пересмешника', '9780446310789', '1960-07-11'),
+    ('Великий Гэтсби', '9780743273565', '1925-04-10'),
+    ('По ком звонит колокол', '9780684803357', '1940-10-21'),
+    ('Прощай, оружие!', '9780684801469', '1929-09-27');
 
 create table book_base_genres(
     id serial primary key,
@@ -118,13 +118,13 @@ create table publishers(
 );
 INSERT INTO publishers (name, description, country_id, contacts)
 VALUES
-    ('Penguin Random House', 'Publishing company known for a wide range of books.', 2, 'contact@penguinrandomhouse.com'),
-    ('HarperCollins Publishers', 'One of the world largest publishing companies.', 2, 'info@harpercollins.com'),
-    ('Simon & Schuster', 'Global publishing company known for bestsellers and literary fiction.', 2, 'info@simonandschuster.com'),
-    ('Macmillan Publishers', 'Global trade publishing company.', 8, 'info@macmillan.com'),
-    ('Hachette Livre', 'Largest publishing company in France and one of the largest in the world.', 3, 'info@hachette.fr'),
-    ('Pearson plc', 'Multinational publishing and education company.', 8, 'contact@pearson.com'),
-    ('Oxford University Press', 'Largest university press in the world.', 8, 'info@oup.com');
+    ('Penguin Random House', 'Издательская компания, известная широким ассортиментом книг.', 2, 'contact@penguinrandomhouse.com'),
+    ('HarperCollins Publishers', 'Одна из крупнейших издательских компаний в мире.', 2, 'info@harpercollins.com'),
+    ('Simon & Schuster', 'Глобальная издательская компания, известная бестселлерами и литературной художественной литературой.', 2, 'info@simonandschuster.com'),
+    ('Macmillan Publishers', 'Глобальная издательская компания в сфере торговли.', 8, 'info@macmillan.com'),
+    ('Hachette Livre', 'Крупнейшая издательская компания во Франции и одна из крупнейших в мире.', 3, 'info@hachette.fr'),
+    ('Pearson plc', 'Многонациональная издательская и образовательная компания.', 8, 'contact@pearson.com'),
+    ('Oxford University Press', 'Крупнейшее университетское издательство в мире.', 8, 'info@oup.com');
 
 create table book_publisher(
     id serial primary key,
@@ -144,13 +144,13 @@ create table languages(
 );
 INSERT INTO languages (full_name, short_name)
 VALUES
-    ('English', 'EN'),
-    ('Spanish', 'ES'),
-    ('French', 'FR'),
-    ('German', 'DE'),
-    ('Chinese', 'ZH'),
-    ('Russian', 'RU'),
-    ('Japanese', 'JA');
+    ('Английский', 'EN'),
+    ('Испанский', 'ES'),
+    ('Французский', 'FR'),
+    ('Немецкий', 'DE'),
+    ('Китайский', 'ZH'),
+    ('Русский', 'RU'),
+    ('Японский', 'JA');
 
 create table book_edition(
     id serial primary key,
@@ -164,30 +164,30 @@ create table book_edition(
 );
 INSERT INTO book_edition (book_publisher_id, release_date, language_id, url, cover_path, description)
 VALUES
-    (1, '2023-01-15', 1, 'lotr1.txt', 'lotr1.png', 'Good book'),
-    (2, '2023-02-20', 2, 'hobbit1.txt', 'hobbit1.png', 'Bad book'),
-    (3, '2023-03-25', 3, 'wap1.txt', 'wap1.png', 'Excellent book'),
-    (4, '2023-04-30', 4, 'ak1.txt', 'ak1.png', 'Romance book'),
-    (5, '2023-05-05', 5, 'hf1.txt', 'hf1.png', 'Happy book'),
-    (6, '2023-06-10', 6, 'ot1.txt', 'ot1.png', 'Sad book'),
-    (7, '2023-07-15', 7, 'tl1.txt', 'tl1.png', 'Nice book'),
-    (8, '2023-08-20', 1, 'mrd1.txt', 'mrd1.png', 'Interesting book'),
-    (9, '2023-09-25', 2, 'km1.txt', 'km1.png', 'Ordinary book'),
-    (10, '2023-10-30', 3, 'gg1.txt', 'gg1.png', 'Captivating book'),
-    (11, '2023-11-05', 4, 'fh1.txt', 'fh1.png', 'Trivial book'),
-    (12, '2023-12-10', 5, 'fa1.txt', 'fa1.png', 'Extraordinary book'),
-    (13, '2024-01-15', 6, 'lotr2.txt', 'lotr2.png', 'Meh book'),
-    (14, '2024-02-20', 7, 'hobbit2.txt', 'hobbit2.png', 'Fine book'),
-    (15, '2024-03-25', 1, 'wap2.txt', 'wap2.png', 'Cool book'),
-    (16, '2024-04-30', 2, 'ak2.txt', 'ak2.png', 'Best book'),
-    (17, '2024-05-05', 3, 'hf2.txt', 'hf2.png', 'Great book'),
-    (18, '2024-06-10', 4, 'ot2.txt', 'ot2.png', 'Banal book'),
-    (19, '2024-07-15', 5, 'tl2.txt', 'tl2.png', 'Super book'),
-    (20, '2024-08-20', 6, 'mrd2.txt', 'mrd2.png', 'Fascinating book'),
-    (1, '2024-09-25', 7, 'lotr3.txt', 'lotr3.png', 'Regular book'),
-    (2, '2024-10-30', 1, 'hobbit3.txt', 'hobbit3.png', 'Mediocre book'),
-    (3, '2024-11-05', 2, 'wap3.txt', 'wap3.png', 'Boring book'),
-    (4, '2024-12-10', 3, 'ak47.txt', 'ak47.png', 'Fantastic book');
+    (1, '2023-01-15', 1, 'lotr1.txt', 'lotr1.png', 'Хорошая книга'),
+    (2, '2023-02-20', 2, 'hobbit1.txt', 'hobbit1.png', 'Плохая книга'),
+    (3, '2023-03-25', 3, 'wap1.txt', 'wap1.png', 'Отличная книга'),
+    (4, '2023-04-30', 4, 'ak1.txt', 'ak1.png', 'Романтическая книга'),
+    (5, '2023-05-05', 5, 'hf1.txt', 'hf1.png', 'Весёлая книга'),
+    (6, '2023-06-10', 6, 'ot1.txt', 'ot1.png', 'Грустная книга'),
+    (7, '2023-07-15', 7, 'tl1.txt', 'tl1.png', 'Милая книга'),
+    (8, '2023-08-20', 1, 'mrd1.txt', 'mrd1.png', 'Интересная книга'),
+    (9, '2023-09-25', 2, 'km1.txt', 'km1.png', 'Обычная книга'),
+    (10, '2023-10-30', 3, 'gg1.txt', 'gg1.png', 'Захватывающая книга'),
+    (11, '2023-11-05', 4, 'fh1.txt', 'fh1.png', 'Банальная книга'),
+    (12, '2023-12-10', 5, 'fa1.txt', 'fa1.png', 'Необычная книга'),
+    (13, '2024-01-15', 6, 'lotr2.txt', 'lotr2.png', 'Так себе книга'),
+    (14, '2024-02-20', 7, 'hobbit2.txt', 'hobbit2.png', 'Неплохая книга'),
+    (15, '2024-03-25', 1, 'wap2.txt', 'wap2.png', 'Крутая книга'),
+    (16, '2024-04-30', 2, 'ak2.txt', 'ak2.png', 'Лучшая книга'),
+    (17, '2024-05-05', 3, 'hf2.txt', 'hf2.png', 'Великолепная книга'),
+    (18, '2024-06-10', 4, 'ot2.txt', 'ot2.png', 'Банальная книга'),
+    (19, '2024-07-15', 5, 'tl2.txt', 'tl2.png', 'Супер книга'),
+    (20, '2024-08-20', 6, 'mrd2.txt', 'mrd2.png', 'Увлекательная книга'),
+    (1, '2024-09-25', 7, 'lotr3.txt', 'lotr3.png', 'Обыденная книга'),
+    (2, '2024-10-30', 1, 'hobbit3.txt', 'hobbit3.png', 'Средняя книга'),
+    (3, '2024-11-05', 2, 'wap3.txt', 'wap3.png', 'Скучная книга'),
+    (4, '2024-12-10', 3, 'ak47.txt', 'ak47.png', 'Фантастическая книга');
 
 create table users(
     id serial primary key,
@@ -196,19 +196,19 @@ create table users(
 );
 INSERT INTO users (first_name, last_name)
 VALUES
-    ('John', 'Doe'),
-    ('Jane', 'Smith'),
-    ('Michael', 'Johnson'),
-    ('Emily', 'Brown'),
-    ('David', 'Jones'),
-    ('Sarah', 'Williams'),
-    ('James', 'Taylor'),
-    ('Emma', 'Wilson'),
-    ('William', 'Davis'),
-    ('Olivia', 'Martinez'),
-    ('Benjamin', 'Anderson'),
-    ('Ava', 'Thomas'),
-    ('Daniel', 'Hernandez');
+    ('Иван', 'Иванов'),
+    ('Мария', 'Смирнова'),
+    ('Алексей', 'Петров'),
+    ('Екатерина', 'Соколова'),
+    ('Дмитрий', 'Михайлов'),
+    ('Анна', 'Федорова'),
+    ('Артем', 'Васильев'),
+    ('Ольга', 'Павлова'),
+    ('Николай', 'Семёнов'),
+    ('Елена', 'Голубева'),
+    ('Сергей', 'Виноградов'),
+    ('София', 'Богданова'),
+    ('Александр', 'Воробьёв');
 
 create table user_edition(
     id serial primary key,
@@ -265,22 +265,22 @@ EXECUTE FUNCTION update_book_edition_rating();
 
 INSERT INTO reviews (user_edition_id, review, rating)
 VALUES
-    (1, 'Great book! Loved the storyline and characters.', 5),
-    (2, 'The plot was engaging, but the ending felt rushed.', 4),
-    (3, 'Disappointing. Expected more from this author.', 2),
-    (4, 'Couldn''t put it down! A must-read for mystery lovers.', 5),
-    (5, 'Not my cup of tea. Slow pacing and uninteresting characters.', 2),
-    (6, 'An engaging story with captivating characters, but the pacing felt slightly uneven', 4),
-    (7, 'Struggled to get through it. Found it boring and predictable.', 2),
-    (8, 'A timeless classic. Beautifully written.', 5),
-    (9, 'Enjoyable read with some unexpected twists.', 4),
-    (10, 'Didn''t live up to the hype. Overrated.', 3),
-    (11, 'Heartwarming story with memorable characters.', 4),
-    (12, 'Thrilling from start to finish. Highly recommend!', 5),
-    (13, 'Interesting premise, but execution fell short.', 3),
-    (14, 'Couldn''t connect with the characters. Story lacked depth.', 2),
-    (15, 'Compelling and thought-provoking. Left me wanting more.', 4),
-    (21, 'Not good book', 1);
+    (1, 'Отличная книга! Понравились сюжет и персонажи.', 5),
+    (2, 'Сюжет был захватывающим, но концовка показалась поспешной.', 4),
+    (3, 'Разочарован. Ожидал большего от этого автора.', 2),
+    (4, 'Не мог отложить! Обязательна к прочтению для любителей тайн.', 5),
+    (5, 'Не моё. Медленное развитие событий и неинтересные персонажи.', 2),
+    (6, 'Захватывающая история с очаровательными персонажами, но темп иногда казался неравномерным', 4),
+    (7, 'Тяжело было дочитать. Нашёл её скучной и предсказуемой.', 2),
+    (8, 'Вечная классика. Прекрасно написано.', 5),
+    (9, 'Приятное чтение с неожиданными поворотами.', 4),
+    (10, 'Не оправдала ожиданий. Переоценена.', 3),
+    (11, 'Теплая история с запоминающимися персонажами.', 4),
+    (12, 'Захватывающая с самого начала и до конца. Настоятельно рекомендую!', 5),
+    (13, 'Интересная предпосылка, но реализация оказалась слабой.', 3),
+    (14, 'Не смог связаться с персонажами. Истории не хватало глубины.', 2),
+    (15, 'Захватывающая и заставляющая задуматься. Оставила желание узнать больше.', 4),
+    (21, 'Плохая книга', 1);
 
 create table user_description(
     id serial primary key,
@@ -294,19 +294,19 @@ create table user_description(
 );
 INSERT INTO user_description (email, password, join_date, bio, country_id, user_id, icon_url)
 VALUES
-    ('1@1.1', MD5('1'), '2023-01-01', 'Bio for User 1', 1, 1, '1.png'),
-    ('2@2.2', MD5('2'), '2023-02-02', 'Bio for User 2', 2, 2, '2.png'),
-    ('3@3.3', MD5('3'), '2023-03-03', 'Bio for User 3', 3, 3, '3.png'),
-    ('user4@example.com', MD5('password4'), '2023-04-04', 'Bio for User 4', 4, 4, '4.png'),
-    ('user5@example.com', MD5('password5'), '2023-05-05', 'Bio for User 5', 5, 5, '5.png'),
-    ('user6@example.com', MD5('password6'), '2023-06-06', 'Bio for User 6', 6, 6, '6.png'),
-    ('user7@example.com', MD5('password7'), '2023-07-07', 'Bio for User 7', 7, 7, '7.png'),
-    ('user8@example.com', MD5('password8'), '2023-08-08', 'Bio for User 8', 1, 8, '8.png'),
-    ('user9@example.com', MD5('password9'), '2023-09-09', 'Bio for User 9', 2, 9, '9.png'),
-    ('user10@example.com', MD5('password10'), '2023-10-10', 'Bio for User 10', 3, 10, '10.png'),
-    ('user11@example.com', MD5('password11'), '2023-11-11', 'Bio for User 11', 4, 11, '11.png'),
-    ('user12@example.com', MD5('password12'), '2023-12-12', 'Bio for User 12', 5, 12, '12.png'),
-    ('user13@example.com', MD5('password13'), '2024-01-01', 'Bio for User 13', 6, 13, '13.png');
+    ('1@1.1', MD5('1'), '2023-01-01', 'Любитель книг и путешествий', 1, 1, '1.png'),
+    ('2@2.2', MD5('2'), '2023-02-02', 'Активный пользователь социальных сетей', 2, 2, '2.png'),
+    ('3@3.3', MD5('3'), '2023-03-03', 'Студент медицинского университета', 3, 3, '3.png'),
+    ('user4@example.com', MD5('password4'), '2023-04-04', 'Маркетолог с опытом работы более 5 лет', 4, 4, '4.png'),
+    ('user5@example.com', MD5('password5'), '2023-05-05', 'Повар, специализирующийся на итальянской кухне', 5, 5, '5.png'),
+    ('user6@example.com', MD5('password6'), '2023-06-06', 'Программист и технологический энтузиаст', 6, 6, '6.png'),
+    ('user7@example.com', MD5('password7'), '2023-07-07', 'Художник, создающий абстрактное искусство', 7, 7, '7.png'),
+    ('user8@example.com', MD5('password8'), '2023-08-08', 'Преподаватель истории', 1, 8, '8.png'),
+    ('user9@example.com', MD5('password9'), '2023-09-09', 'Специалист по цифровой безопасности', 2, 9, '9.png'),
+    ('user10@example.com', MD5('password10'), '2023-10-10', 'Дизайнер интерьеров', 3, 10, '10.png'),
+    ('user11@example.com', MD5('password11'), '2023-11-11', 'Фотограф природы', 4, 11, '11.png'),
+    ('user12@example.com', MD5('password12'), '2023-12-12', 'Ветеринар с 10-летним опытом', 5, 12, '12.png'),
+    ('user13@example.com', MD5('password13'), '2024-01-01', 'Музыкант и композитор', 6, 13, '13.png');
 
 
 create table bookmarks(
@@ -330,13 +330,13 @@ create table status(
 );
 INSERT INTO status (status)
 VALUES
-    ('Reading'),
-    ('To Read'),
-    ('Completed'),
-    ('Paused'),
-    ('Abandoned'),
-    ('Reviewing'),
-    ('Planning');
+    ('Читаю'),
+    ('Буду читать'),
+    ('Прочитано'),
+    ('На паузе'),
+    ('Брошено'),
+    ('Оставляю отзыв'),
+    ('Планирую');
 
 create table book_status(
     id serial primary key,
